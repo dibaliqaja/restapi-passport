6e8ce957cc0322a8755ef43f37dbde229c5eaec7
Iqbal <dibaliqaja@gmail.com>
2020-05-08 14:47:47 +0700

    #5 - Create API Routes

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('register', 'API\RegisterController@register');
+Route::post('login', 'API\RegisterController@login');
+
+Route::middleware('auth:api')->group( function() {
+    Route::resource('products', 'API\ProductController');
 });
